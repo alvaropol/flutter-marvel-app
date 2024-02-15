3f5813d981d22c6786da921c56b270dcb96b855f
alvaropol <alvaro.pold@gmail.com>
2024-02-15 09:46:15 +0100

    styles changued and more fields added to list and details character

--- flutter_application_marvel/lib/ui/screens/characters_screen.dart
+++ flutter_application_marvel/lib/ui/screens/characters_screen.dart
@@ -4,14 +4,10 @@ import 'package:flutter_application_marvel/repositories/characters/character_rep
 import 'package:flutter_application_marvel/repositories/characters/character_repository_impl.dart';
 import 'package:flutter_application_marvel/ui/screens/character_detail_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:getwidget/components/avatar/gf_avatar.dart';
-import 'package:getwidget/components/button/gf_button.dart';
-import 'package:getwidget/components/button/gf_button_bar.dart';
-import 'package:getwidget/components/card/gf_card.dart';
-import 'package:getwidget/components/list_tile/gf_list_tile.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CharactersScreen extends StatefulWidget {
-  const CharactersScreen({super.key});
+  const CharactersScreen({Key? key}) : super(key: key);
 
   @override
   State<CharactersScreen> createState() => _CharactersScreenState();
@@ -33,13 +29,14 @@ class _CharactersScreenState extends State<CharactersScreen> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider.value(
-        value: _characterBloc,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Characters of Marvel'),
-          ),
-          body: _characterList(),
-        ));
+      value: _characterBloc,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Characters of Marvel'),
+        ),
+        body: _characterList(),
+      ),
+    );
   }
 
   Widget _characterList() {
@@ -47,72 +44,155 @@ class _CharactersScreenState extends State<CharactersScreen> {
       children: [
         if (offset != 0)
           ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  offset -= 20;
+                });
+                _characterBloc = CharactersBlocBloc(characterRepository)
+                  ..add(CharacterFetchList(offset));
+              },
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)),
+              child: const Text('Return to the other characters',
+                  style: TextStyle(color: Colors.white))),
+        const SizedBox(height: 20),
+        ElevatedButton(
             onPressed: () {
               setState(() {
-                offset -= 20;
+                offset += 20;
               });
               _characterBloc = CharactersBlocBloc(characterRepository)
                 ..add(CharacterFetchList(offset));
             },
-            child: const Text('Return to the other characters'),
-          ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              offset += 20;
-            });
-            _characterBloc = CharactersBlocBloc(characterRepository)
-              ..add(CharacterFetchList(offset));
-          },
-          child: const Text('Show next characters'),
-        ),
+            style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.black)),
+            child: const Text('Show next characters',
+                style: TextStyle(color: Colors.white))),
         Expanded(
           child: BlocBuilder<CharactersBlocBloc, CharactersBlocState>(
             builder: (context, state) {
               if (state is CharacterFetchSuccess) {
-                return ListView.builder(
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 10.0,
+                  ),
                   itemCount: state.characterList.length,
                   itemBuilder: (context, index) {
                     String image =
                         "${state.characterList[index].thumbnail!.path}.${state.characterList[index].thumbnail!.extension}";
                     return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(image),
-                            fit: BoxFit.cover,
-                          ),
-                          color: Colors.white.withOpacity(0.5),
+                      padding: const EdgeInsets.all(7.0),
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
                         ),
-                        child: GFCard(
-                          title: GFListTile(
-                            avatar: GFAvatar(
-                              backgroundImage: NetworkImage(image),
-                            ),
-                            title: Text(state.characterList[index].name!),
-                          ),
-                          content: Text(
-                              'Modified: ${state.characterList[index].modified!}'),
-                          buttonBar: GFButtonBar(
-                            children: <Widget>[
-                              GFButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          DetailCharacterScreen(
-                                        characterId:
-                                            state.characterList[index].id!,
-                                        nameCharacter:
-                                            state.characterList[index].name!,
-                                      ),
+                        child: ClipRect(
+                          child: Stack(
+                            children: [
+                              const Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  image: DecorationImage(
+                                    image: NetworkImage(image),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15.0),
+                                    color: Colors.black.withOpacity(0.6),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Stack(
+                                      children: [
+                                        Column(
+                                          children: [
+                                            ListTile(
+                                              title: Text(
+                                                state
+                                                    .characterList[index].name!,
+                                                style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(5.0),
+                                              child: Text(
+                                                '${state.characterList[index].name!} appears in  ${state.characterList[index].stories?.available} stories',
+                                                style: const TextStyle(
+                                                    color: Colors.white),
+                                              ),
+                                            ),
+                                            GestureDetector(
+                                              onTap: () {
+                                                String? wikiUrl = state
+                                                    .characterList[index]
+                                                    .urls?[2]
+                                                    .url;
+                                                if (wikiUrl != null) {
+                                                  launchUrl(wikiUrl as Uri);
+                                                }
+                                              },
+                                              child: const Text(
+                                                'Comics URL',
+                                                style: TextStyle(
+                                                  color: Colors.blue,
+                                                  decoration:
+                                                      TextDecoration.underline,
+                                                ),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.all(15.0),
+                                              child: ElevatedButton(
+                                                style: ButtonStyle(
+                                                    backgroundColor:
+                                                        MaterialStatePropertyAll(
+                                                            Colors.white
+                                                                .withOpacity(
+                                                                    0.7))),
+                                                onPressed: () {
+                                                  Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                      builder: (context) =>
+                                                          DetailCharacterScreen(
+                                                        characterId: state
+                                                            .characterList[
+                                                                index]
+                                                            .id!,
+                                                        nameCharacter: state
+                                                            .characterList[
+                                                                index]
+                                                            .name!,
+                                                      ),
+                                                    ),
+                                                  );
+                                                },
+                                                child: const Text(
+                                                  'View details',
+                                                  style: TextStyle(
+                                                      color: Colors.black,
+                                                      fontSize: 14),
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ],
                                     ),
-                                  );
-                                },
-                                text: 'Details',
+                                  ),
+                                ),
                               ),
                             ],
                           ),
